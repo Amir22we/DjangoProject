30b80445c0eee84a158558eee0b202c71cf84c11
Амир <atazanovamir19@gmail.com>
2025-10-23 21:18:53 +0500

    Диспетчер url. 23.10.2025

---
+++ blog/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('<h2>Главная</h2>')
+
+def about(request, name, age):
+    return HttpResponse(f'''
+                        <h2>О пользователе</h2>
+                        <p>Имя: {name}</p>
+                        <p>Возраст: {age}</p>
+                        ''')
+
+def contact(request):
+    return HttpResponse('<h2>Контакты</h2>')
+
+def user(request, name='Undefined', age=0):
+    return HttpResponse(f'<h2>Имя: {name} ||  Возраст: {age}</h2>')
+
+def winner(request, name='Undefined', price=0):
+    return HttpResponse(f'<h2>Победитель: {name} || Приз: {price}$  </h2>')
+
